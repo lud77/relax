02cf40bd1cf999b9529cb5c31de1c037d0bc1e0c
Stefano Butera <stefano.butera@trinitymirror.com>
2018-04-11 17:42:38 +0100

    Implement basic interface

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const { set: setProp, delete: deleteProp } = require('dot-prop-immutable');
+const murmur = require('murmur-hash-js');
+
+const hash = (key) => murmur(JSON.stringify(key), 0);
+
+module.exports = () => {
+    let state = {};
+
+    const views = [];
+
+    const updateView = (view) => {
+        const newView = view.transform(state);
+        const newHash = hash(newView);
+        if (view.hash !== newHash) {
+            view.hash = newHash;
+            view.listener(newView);
+        }
+    };
+
+    const updateViews = () => views.map(updateView);
+
+    const createView = (transform, listener) => {
+        views.push({
+            transform,
+            listener,
+            hash: -1
+        });
+    };
+
+    const set = (path, value) => {
+        state = setProp(state, path, value);
+        updateViews();
+    };
+
+    const del = (path) => {
+        state = deleteProp(state, path);
+        updateViews();
+    };
+
+    return {
+        init,
+        set,
+        del,
+        createView
+    };
+};
